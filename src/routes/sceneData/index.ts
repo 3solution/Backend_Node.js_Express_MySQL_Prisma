ec32e4674ee55b18233d831c401b0bd97b5a3254
Evil-Developer <pro.expert0525@gmail.com>
2022-11-19 05:34:33 +0200

    Fix: api issue

--- src/routes/sceneData/index.ts
+++ src/routes/sceneData/index.ts
@@ -3,7 +3,7 @@ import { saveData, importData, getNames } from "./handler";
 
 const sceneDataRouter = Router();
 sceneDataRouter.post("/save/:uid", saveData);
-sceneDataRouter.post("/import/:uid", importData);
+sceneDataRouter.get("/import/:uid", importData);
 sceneDataRouter.get("/names/:uid", getNames);
 
 export default sceneDataRouter;
